d1aff0d8770c1fa64e155d9d5a15bcddfb91d54a
RAJA D G <dgraja@ssdi.sharp.co.in>
2016-05-06 22:03:34 +0530

    django01: implement CRUD in contacts list
    1. updated bootstrap template
    2. designed UI for contacts page
    3. implemented client-server crud for contacts

--- django01/application/simple/database.py
+++ django01/application/simple/database.py
@@ -4,7 +4,7 @@ Created on 04-May-2016
 @author: dgraja
 '''
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django01.settings import BASE_DIR
 from application import models
 
@@ -13,29 +13,57 @@ def contact_view(request):
     """
         displays contact view
     """
-    name = email = phone = ""
+    data = {"name": "",
+            "email": "",
+            "phone": "",
+            "target": "",
+            "action": "",
+            "activity": "",
+            "action_icon": "glyphicon-floppy-disk"}
     if request.POST:
-        if "name" in request.POST:
-            name = request.POST["name"]
-        if "email" in request.POST:
-            email = request.POST["email"]
-        if "phone" in request.POST:
-            phone = request.POST["phone"]
-        if "add" in request.POST and request.POST["add"] == "Add":
-            ct = models.Contact()
-            ct.name = name
-            ct.email = email
-            ct.phone = phone
-            ct.save()
+        for key in data.keys():
+            if key in request.POST:
+                data[key] = request.POST[key]
+    if data["activity"] == "":
+        data["activity"] = "add"
+
+    if data["action"] == "add" and data["activity"] == "add":
+        posted = models.Contact()
+        posted.name = data["name"]
+        posted.email = data["email"]
+        posted.phone = data["phone"]
+        posted.save()
+    if data["action"] == "add" and data["activity"] == "save":
+        posted = models.Contact.objects.get(id=int(data["target"]))
+        posted.name = data["name"]
+        posted.email = data["email"]
+        posted.phone = data["phone"]
+        posted.save()
+    elif data["action"] == "edit" and data["target"]:
+        data["activity"] = "save"
+        to_edit = models.Contact.objects.get(id=int(data["target"]))
+        data["name"] = to_edit.name
+        data["phone"] = to_edit.phone
+        data["email"] = to_edit.email
+    elif data["action"] == "remove" and data["target"]:
+        to_edit = models.Contact.objects.filter(id=int(data["target"]))
+        to_edit.delete()
+    elif request.POST:
+        redirect("/index")
+
+    if data["action"] != "edit":
+        data["name"] = data["phone"] = data["email"] = ""
+        data["target"] = 0
+        data["activity"] = "Add"
+        data["action_icon"] = "glyphicon-plus"
+
 
     def make_contact(row):
-        return {'name': row.name, "id": row.id, "email": row.email, "phone": row.phone}
+        make_contact.counter = make_contact.counter + 1
+        return {'name': row.name, "id": row.id, "email": row.email, "phone": row.phone, "sno": make_contact.counter}
+    make_contact.counter = 0
 
     rows = list([make_contact(row) for row in models.Contact.objects.all()])
-    data = {"name": name,
-            "email": email,
-            "phone": phone,
-            "items": rows}
-
+    data["items"] = rows
 
     return render(request, "simple/contacts.html", data)
